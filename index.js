d697dfa581399d433c2460129d3a447410f7c744
Austin <austin.inhyuk.chong@gmail.com>
2025-01-24 13:34:47 -0800

    Add numbers to display
    Can now display numbers and the clear button works. Need to know how
    to only display buttons and not the container.

--- index.js
+++ index.js
@@ -23,4 +23,14 @@ let a;
 let b;
 let op = divide;
 
-console.log(operate(op, 1, 2));
+// console.log(operate(op, 1, 2));
+const display = document.querySelector(".display");
+const container = document.querySelector(".container")
+
+container.addEventListener("click", (element) => {
+    const btn =  element.target.classList;
+    display.textContent = display.textContent + btn;
+    if (btn == "clear") {
+        display.textContent = "";
+    }
+})
