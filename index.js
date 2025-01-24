df718feaed54aeb71edd94b3789cee6ce1e1f1b5
Austin <austin.inhyuk.chong@gmail.com>
2025-01-24 14:49:53 -0800

    Working Calculator
    Calculator is now able to finish simple operations. Need to be able to
    clear the screen once the next button is pushed.

--- index.js
+++ index.js
@@ -16,23 +16,36 @@ function divide(a, b){
 }
 
 function operate(op, a, b){
-    return op(1, 2);
+    if (op == "+") op = add;
+    if (op == "-") op = sub;
+    if (op == "*") op = mult;
+    if (op == "/") op = divide;
+    return op(a, b);
 }
 
 let a;
 let b;
-let op = divide;
+let op;
 
 // console.log(operate(op, 1, 2));
 const display = document.querySelector(".display");
 const container = document.querySelector(".container")
 
 container.addEventListener("click", (element) => {
-    const btn =  element.target.classList;
+    const btn =  element.target.id;
+    const classList = element.target.classList;
     if (element.target.tagName == "BUTTON"){
-        display.textContent = display.textContent + btn;
         if (btn == "clear") {
             display.textContent = "";
+        } else if (classList == "operator"){
+            a = display.textContent;
+            op = btn;
+            display.textContent = "";
+        } else if (btn == "="){
+            b = display.textContent;
+            display.textContent = operate(op, a, b);
+        } else {
+            display.textContent = display.textContent + btn;
         }
     }
 })
